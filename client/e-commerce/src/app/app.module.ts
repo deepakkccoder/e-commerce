5bb2180e46f1bcd682638b4b41f1e64e98a486f4
Deepak K C <deepakkc.2045@gmail.com>
2017-11-12 20:11:55 -0600

    e-commerce client using angular 4

---
+++ client/e-commerce/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CategoryCreateComponent } from './category/category-create/category-create.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoryCreateComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/e-commerce/src/app/category/category-create/category-create.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-category-create',
+  templateUrl: './category-create.component.html',
+  styleUrls: ['./category-create.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CategoryCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
